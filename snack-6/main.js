035022d52aa99c34a2765f1cc5aa682d53350e07
GabriPini <lele19977@gmail.com>
2022-03-30 13:49:21 +0200

    add snack-6

---
+++ snack-6/main.js
@@ -0,0 +1,12 @@
+/*Snack-6 Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente. */
+
+//faccio scegliere un numero all utente 
+let number = parseInt(prompt('inserisci un numero'));
+// inposto il 3 come esponente per calcolare il cubo
+let exponent = 3;
+
+//eseguo l'operazione
+console.log( Math.pow(number, exponent));
+
+    
