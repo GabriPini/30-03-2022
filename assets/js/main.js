50d7ef669dc189819f762b4481c3bc2750d1e028
GabriPini <lele19977@gmail.com>
2022-03-30 10:24:03 +0200

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,22 +6,22 @@
  if (isNaN(firstNumber)) {
    alert('😱 Devi inserire un numero')
  }
- //chiedere all'utente età del passeggero.
+
  const secondNumber = parseInt(prompt('Inserisci il secondo numero'));
  if (isNaN(secondNumber)) {
    alert('😱 Devi inserire un numero p')
  }
 
  if (firstNumber > secondNumber){
-    // user wins
+   
     console.log(`il primo numero da te inserito è il maggiore  ${firstNumber}`) 
 
 } else if ( firstNumber < secondNumber){
-    // user lose
+    
     console.log(`il secondo numero da te inserito è il maggiore  ${secondNumber}`)
 
 } else {
-    // It's a draw
+    
     console.log('sono due numeri uguali')
 
 }
