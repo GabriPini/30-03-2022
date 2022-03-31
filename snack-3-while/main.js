3856ca99f5e7224bab6a3d71e030ce98262d6a0e
GabriPini <lele19977@gmail.com>
2022-03-31 11:51:56 +0200

    add snack-3-while

---
+++ snack-3-while/main.js
@@ -0,0 +1,29 @@
+/* 
+SNACK 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+
+ */
+
+let result, userNumber;
+result = 0;
+let i = 1;
+
+while( i <= 10){
+    
+                userNumber = parseInt(prompt('inserisci un numero'))
+                result = result + userNumber;
+                i++;
+      
+                }
+
+console.log(`la somma è ${result}`)
+                
+
+
+            
+
+    
+                
+ 
+ 
